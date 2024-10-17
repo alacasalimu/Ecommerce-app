454afbd771668f5459de161315a4a9887ac91bc1
alacasalimu <a.mutekin@gmail.com>
2024-10-17 16:25:13 +0300

    configure a package

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Collection from "./pages/Collection";
 import About from "./pages/About";
