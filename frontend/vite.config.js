4c3f034fed09af2bb9563a55e1dca9a3f805ef12
alacasalimu <a.mutekin@gmail.com>
2024-10-18 14:31:38 +0300

    configured vite file for deployment conflict 6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: [
-        "react-router-dom",
+        "./react-router-dom",
         "react-toastify",
         "react-toastify/dist/ReactToastify.css",
         "axios",
